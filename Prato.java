db38c1609cdbead681ef2f5e6ee0050dd79f703f
Joaotds <109186798+joaotds@users.noreply.github.com>
2023-06-13 15:55:19 -0300

    Create Prato.java

---
+++ Prato.java
@@ -0,0 +1,17 @@
+public class Prato {
+    private String nome;
+    private double preco;
+
+    public Prato(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
